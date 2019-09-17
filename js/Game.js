cdc9e4b3eb166ad8cf80402c85bb07a7d0f02caa
fullop2 <parkth32@gmail.com>
2019-09-17 09:16:13 +0900

    class

--- js/Game.js
+++ js/Game.js
@@ -10,37 +10,31 @@ $(document).keyup(function(e){ // 눌렸던 키를 해제
 
 function Game(){
 
-    this._scene = new THREE.Scene();
+    this._scene=new THREE.Scene();
     this._camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
     this._renderer = new THREE.WebGLRenderer({antialias:true});
     this._renderer.setSize(window.innerWidth,window.innerHeight);
     this._renderer.setClearColor (0xcccccc, 1);
+    this._loader = new THREE.GLTFLoader();
 
-    this._loader= new THREE.GLTFLoader();
+    this.animate =()=>{
+      requestAnimationFrame(this.animate);
+      this._renderer.render(this._scene,this._camera);
+    }
 
-
-    this.modelLoader = (dir)=>{
-      var obj = null;
+    this.modelLoader=(dir)=>{
       this._loader.load(
         dir,
         (gltf)=>{
-          gltf.animations;
-          gltf.scene;
-          gltf.scenes;
-          gltf.cameras;
-          gltf.asset;
-          obj = gltf.scene;
-          this._scene._objects.push(obj);
-          this._scene.add(obj);
+          this._scene.add(gltf.scene);
         },
         (xhr)=>{
           console.log((xhr.loaded / xhr.total*100)+"% loaded");
         },
         (error)=>{
-          console.log('An error happened');
+          console.log('An error happened : ' + error);
         }
       );
-      console.log(this.obj);
       return this.obj;
     }
 }
